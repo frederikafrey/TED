c4edef798c3755494e89e0de5356b8f61fc6a86e
Frederika Frey <frfrey@ttu.ee>
2020-09-11 16:07:04 +0300

    testid

--- PointTests/UnitTest1.cs
+++ PointTests/UnitTest1.cs
@@ -19,8 +19,27 @@ namespace PointTests
         }
 
         [TestMethod]
-        public void TestMethod1()
+        public void XTest()
         {
+            var p1X = p1.getX();
+            Assert.IsNotNull(p1X);
+            Assert.AreEqual(p1X, 0);
+        }
+
+        [TestMethod]
+        public void YTest()
+        {
+            var p1Y = p1.getY();
+            Assert.IsNotNull(p1Y);
+            Assert.AreEqual(p1Y, 0);
+        }
+
+        [TestMethod]
+        public void GetDistanceTest()
+        {
+            double actual = Math.Sqrt(Math.Pow(p1.getX() - p2.getX(), 2) + Math.Pow(p1.getY() - p2.getY(), 2));
+            double expected = p1.GetDistance(p2);
+            Assert.AreEqual(expected, actual);
         }
     }
 }
